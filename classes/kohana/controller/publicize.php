74186a12741cee7e24611125adacd2f14b23942b
Luke Morton <lukemorton.designs@gmail.com>
2011-11-02 11:45:57 +0000

    Tidy up Kohana_Controller_Publicize.

--- classes/kohana/controller/publicize.php
+++ classes/kohana/controller/publicize.php
@@ -11,11 +11,11 @@
 class Kohana_Controller_Publicize extends Controller {
 	
 	/**
-	 * When not in development mode this action will copy the
-	 * file into DOCROOT and redirect the request to the static
-	 * file. However in development mode the file will be served
-	 * by Kohana to allow for faster development.
-	 * 
+	 * When Kohana::$environment is set to Kohana::DEVELOPMENT
+	 * the asset will be served from this Controller.
+	 * Otherwise the file will be served here once and then
+	 * copied to DOCROOT and subsequently served from there.
+	 *
 	 * @return  void
 	 */
 	public function action_copy()
@@ -25,26 +25,20 @@ class Kohana_Controller_Publicize extends Controller {
 		if (Publicize::should_copy_to_docroot())
 		{
 			$uri = $this->request->param('uri');
-			
-			// Copy and redirect
 			Publicize::copy_to_docroot($asset, $uri);
-			//$this->request->redirect($uri);
 		}
 
-    // Find the file extension
-    $extension = pathinfo($asset, PATHINFO_EXTENSION);
+		$extension = pathinfo($asset, PATHINFO_EXTENSION);
 
-    // Prepare header
-    // Bug in 3.2 prevents 'Content-Type' to work as expected -> 'content-type'
-    // see: https://github.com/kohana/core/commit/4605ccb6957a7b3a9854792328c937d1db003502
-    $this->response->headers(array(
-        'content-type'   => File::mime_by_ext($extension),
-        'Content-Length' => (string) filesize($asset),
-        'Last-Modified'  => date(DATE_RFC1123, filemtime($asset)),
-    ));
+		// Bug in 3.2 prevents 'Content-Type' to work as expected -> 'content-type'
+		// see: https://github.com/kohana/core/commit/4605ccb6957a7b3a9854792328c937d1db003502
+		$this->response->headers(array(
+			'content-type'   => File::mime_by_ext($extension),
+			'Content-Length' => (string) filesize($asset),
+			'Last-Modified'  => date(DATE_RFC1123, filemtime($asset)),
+		));
 
-    // Serve the resource
-    $this->response->body(file_get_contents($asset));
+		$this->response->body(file_get_contents($asset));
 	}
 	
 }
